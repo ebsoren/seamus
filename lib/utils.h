af0171b610e50e868373ca8d3e07167f95f43c05
Aiden Mizhen <aiden@boriska.com>
2026-02-11 16:04:37 -0500

    Created utils.h and wrote stem_word. good to finalize this early on since it cannot change once indexing begins

---
+++ lib/utils.h
@@ -0,0 +1,43 @@
+//
+// Created by Aiden Mizhen on 2/10/26.
+//
+#pragma once
+
+#include "string.h"
+
+
+void stem_word(string& word) {
+    bool double_check = false;
+    if (word.ends_with("ing") and word.size() >= 6) {
+        word.resize(word.size() - 3);
+        double_check = true;
+    }
+    else if (word.ends_with("ed") and word.size() >= 5) {
+        word.resize(word.size() - 2);
+        double_check = true;
+    }
+    else if (word.ends_with("ly") and word.size() >= 5) {
+        word.resize(word.size() - 2);
+    }
+    else if (word.ends_with("ful") and word.size() >= 6) {
+        word.resize(word.size() - 3);
+    }
+    else if (word.ends_with("ness") and word.size() >= 7) {
+        word.resize(word.size() - 4);
+    }
+    else if (word.ends_with("es") and word.size() >= 5) {
+        word.resize(word.size() - 2);
+    }
+    else if (word.ends_with("s") and word.size() >= 4) {
+        word.resize(word.size() - 1);
+    }
+    else if (word.ends_with("er") and word.size() >= 7) {
+        word.resize(word.size() - 2);
+    }
+    if (double_check) {
+        if (word[word.size()-1] == word[word.size()-2] and
+            word[word.size()-1] != 'l' and word[word.size()-1] != 's' and word[word.size()-1] != 'z') {
+            word.resize(word.size() - 1);
+            }
+    }
+}
