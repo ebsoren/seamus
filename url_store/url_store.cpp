4ab3698024ade253f13c1de0c3d07ab0c2ccc5a5
charlesthuang <chuang6703@gmail.com>
2026-02-18 22:34:43 -0500

    fixes url_data traversal in UrlStore::persist()

--- url_store/url_store.cpp
+++ url_store/url_store.cpp
@@ -1,4 +1,5 @@
 #include "url_store.h"
+#include "lib/string.h"
 
 
 const UrlData* UrlStore::findUrlData(const string& url) const {
@@ -97,7 +98,9 @@ void UrlStore::persist() {
         fwrite("\n", sizeof(char), 1, fd);
     }
     
-    for (const auto& [url, data] : url_data) {
+    for (const auto& slot : url_data) {
+        const string& url = slot.key;
+        const UrlData& data = slot.value;
         fprintf(fd, "%lu", url.size());
         fwrite(&url, sizeof(char), url.size(), fd);
         fprintf(fd, "%u %u %u %u %u\n", data.num_encountered, data.seed_distance, data.eot, data.eod, data.anchor_freqs.size());
@@ -121,7 +124,7 @@ void UrlStore::readFromFile(UrlStore& url_store, const int worker_number) {
     for (uint32_t i = 0; i < num_anchor_texts; i++) {
         uint32_t anchor_text_len;
         fread(&anchor_text_len, sizeof(uint32_t), 1, fd);
-        string anchor_text(anchor_text_len, '\0');
+        string anchor_text(anchor_text_len, '\0'); // TODO: revisit after friday
         fread(&anchor_text[0], sizeof(char), anchor_text_len, fd);
         url_store.anchor_to_id.push_back(anchor_text);
         fread(&dummy, sizeof(char), 1, fd); // consume newline
@@ -129,7 +132,7 @@ void UrlStore::readFromFile(UrlStore& url_store, const int worker_number) {
 
     uint32_t url_len;
     while (fread(&url_len, sizeof(uint32_t), 1, fd)) {
-        string url(url_len, '\0');
+        string url(url_len, '\0'); // TODO: revisit after friday
         fread(&url[0], sizeof(char), url_len, fd);
         url_store.url_data[url] = UrlData();
 
