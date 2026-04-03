0846338568c27de749f6fc23fd52cda0d119be8a
Aiden Mizhen <aiden@boriska.com>
2026-04-03 19:17:43 -0400

    cleaned up some code

--- index/Index.cpp
+++ index/Index.cpp
@@ -86,12 +86,13 @@ void IndexChunk::persist() {
 
         // Used to fill the internal index at the top of a posting list, which maps doc ID to byte offset from start of index
         uint64_t internal_offset = 0;
-        uint32_t num_docs = index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())].posts[0].doc == 0 ? 1 : 0;
+        postings& entry = index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())];
+        uint32_t num_docs = entry.posts[0].doc == 0 ? 1 : 0;
 
         // 32 bit ID, 64 bit offset, 2 filler chars per entry
         const uint64_t INTERNAL_INDEX_ENTRY_SIZE = 5 + 6 + 2;
 
-        for (post p : index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())].posts) {
+        for (post p : entry.posts) {
             // Utf8 encoding size of loc offset (no delimiters)
             uint64_t post_size = SizeOfUtf8(p.loc - last_loc);
 
@@ -150,17 +151,18 @@ void IndexChunk::persist() {
 
     // Loop through all words
     for (uint32_t i = 0; i < N; i++) {
-        uint64_t size = index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())].posts.size(); // Needs to be an lvalue for fwrite
+        postings& entry = index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())];
+        uint64_t size = entry.posts.size(); // Needs to be an lvalue for fwrite
 
         // Write the number of occurrences and documents
         // <64b NUM POSTS> <64b NUM DOCS>\n
         fwrite(&size, sizeof(uint64_t), 1, fd);
         fwrite(" ", sizeof(char), 1, fd);
-        fwrite(&index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())].n_docs, sizeof(uint64_t), 1, fd);
+        fwrite(&entry.n_docs, sizeof(uint64_t), 1, fd);
         fwrite("\n", sizeof(char), 1, fd);
 
         // Write the internal index
-        // For each document that appears in this posting list: <32b DOC ID> <64b BYTE OFFSET FROM START OF INTERNAL INDEX>\n 
+        // For each document that appears in this posting list: <32b DOC ID> <64b BYTE OFFSET FROM START OF INTERNAL INDEX>\n
         for (uint32_t j = 0; j < curr_doc_ + 1; j++) {
             // Skip docs that don't appear
             if (doc_offsets[i][j] == UINT32_MAX) continue;
@@ -182,7 +184,7 @@ void IndexChunk::persist() {
 
         // For each word occurrence: (IF NEW DOC <0b0||varlen DOC OFFSET>)<varlen LOC ID/OFFSET>
         // Because we're doing UTF 8, no delimiters
-        for (post p : index[alphabetized_entries[i].str_view(0, alphabetized_entries[i].size())].posts) {
+        for (post p : entry.posts) {
             // Only write the doc offset if it's a new document, in which case write the offset after the flag
             if (p.doc > last_doc) {
                 Utf8* doc_end = WriteUtf8(doc_buff + 1, p.doc - last_doc, doc_buff + MAX_UTF8_LEN + 1);
