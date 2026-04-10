7f793d2786e3b24eb85847cf24a7092278a38c8c
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2026-04-10 00:03:02 -0400

    default to 1 machine--localhost on main branch, keep environment variable-based machine ID in consts

--- lib/consts.h
+++ lib/consts.h
@@ -11,7 +11,7 @@ constexpr const char* USER_AGENT = "Seamus the Search Engine (web crawler for un
 
 // Global
 constexpr size_t NUM_MACHINES = 1;
-constexpr const char* MACHINES[NUM_MACHINES] = {"136.119.122.181"};
+constexpr const char* MACHINES[NUM_MACHINES] = {"127.0.0.1"};
 
 inline const char* get_machine_addr(size_t machine_id) {
     assert(machine_id < NUM_MACHINES);
