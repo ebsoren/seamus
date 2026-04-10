27ba8d5e630e9b8bd9afaf8fca3609e293b4837c
DMcDe <dmcde@umich.edu>
2026-04-10 08:26:36 -0400

    finish loadedindex init (untested), TODO: refactor ISR constructor to take LoadedIndex instead of path

--- index-stream-reader/isr.cpp
+++ index-stream-reader/isr.cpp
@@ -9,9 +9,6 @@ LoadedIndex::LoadedIndex(string path) {
     urls.resize(DOCS_PER_INDEX_CHUNK);
     dictionary.reserve(DOCS_PER_INDEX_CHUNK);
 
-    // TODO: This is huge. Is heap allocating like this alright?
-    posting_list_ = new uint8_t[POSTING_LIST_BUFFER_SIZE];
-
     FILE* fd;
     if ((fd = fopen(path.data(), "rb")) == nullptr) {
         logger::warn("Failed to open %s", path);
@@ -22,17 +19,52 @@ LoadedIndex::LoadedIndex(string path) {
     uint64_t urls_size;
     fread(&urls_size, sizeof(uint64_t), 1, fd);
 
-    char url_buff[4096];
+    char buff[4096];
     uint64_t bytes_read = 0;
+
+    uint32_t id;
     while (bytes_read < urls_size) {
-        fgets(url_buff, sizeof(url_buff), fd);
-        bytes_read += strlen(url_buff);
-        // TODO: Parse <32b ID> <varlen URL>\n and add to urls vector 
+        fgets(buff, sizeof(buff), fd);
+        bytes_read += strlen(buff);
+
+        // Parse <32b ID> <varlen URL>\n and add to urls vector
+        // The URL starts after 4B Id and 1B char, and the total length is 4B + 1B + strlen + 1B
+        memcpy(&id, buff, sizeof(uint32_t));
+        urls[id] = string(buff + sizeof(uint32_t) + 1, strlen(buff) - sizeof(uint32_t) - 2);
     }
+    
+    // Should now be at the top of the dict table of contents
+    // Validate this by checking that we see the char 'a'
+    char ch;
+    fread(&ch, sizeof(char), 1, fd);
+    if (ch != 'a') logger::warn("Error when initializing %s: Not at expected start of dict contents.", path);
 
     // Populate the dictionary
+    // Seek precisely to start of dictionary
+    if (fseek(fd, INDEX_DICTIONARY_SIZE + urls_size, SEEK_SET) != 0) logger::warn("Error when initializing %s: Seek to dictionary failed.", path);
+
+    uint64_t posting_list_offset;
+
+    do {
+        fgets(buff, sizeof(buff), fd);
+        
+        // Copy from end of buffer minus 1B for \n - 4B for offset to get start of offset
+        memcpy(&posting_list_offset, buff + strlen(buff) - (1 + sizeof(uint64_t)), sizeof(uint64_t));
+
+        // Insert offset into dictionary
+        dictionary.insert(string(buff, strlen(buff) - (1 + sizeof(uint64_t))), posting_list_offset);
+    } while (strcmp(buff, "\n"));
+
+    // Now at the top of the skip list
 
     // Populate the posting list
+    // TODO: This is huge. Is heap allocating like this alright?
+    posting_list_ = new uint8_t[POSTING_LIST_BUFFER_SIZE];
+
+    // Read until the end of file, and assert that end of file was reached
+    fread(posting_list_, sizeof(uint8_t), UINT64_MAX, fd);
+
+    if (feof(fd) == 0) logger::warn("Failed to reach end of file when loading %s posting list.", path);
 
     fclose(fd);
 }
@@ -41,6 +73,7 @@ LoadedIndex::~LoadedIndex() {
     delete[] posting_list_;
 }
 
+// TODO: Refactor this to take in a LoadedIndex object instead of chunk_path
 IndexStreamReader::IndexStreamReader(string word, string chunk_path) : word(move(word)) {
     struct stat buff;
     if (stat(chunk_path.data(), &buff) == -1) {
